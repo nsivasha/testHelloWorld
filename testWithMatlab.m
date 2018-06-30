6b8d2702405fbe3323fc5e126714dd0c2c0349f0
nsivasha <36686424+nsivasha@users.noreply.github.com>
2018-06-29 21:49:32 -0400

    fourth commit after the add command.

--- testWithMatlab.m
+++ testWithMatlab.m
@@ -2,3 +2,10 @@
 
 % adding a second line for a second commit
 
+% trying a third line commit from MATLAB with correct password.
+
+% fourth change to see if the password prompt appears again.
+
+
+
+
